1b7f77a0a1fc1b079c72d5693eb9fac4ec422e17
Max <max.dupenois@gmail.com>
2016-01-03 19:26:21 +0000

    Added timestamps to rsvps

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016210956) do
+ActiveRecord::Schema.define(version: 20160103185321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "rsvps", force: :cascade do |t|
-    t.string  "name"
-    t.text    "extra"
-    t.integer "response", default: 0
+    t.string   "name"
+    t.text     "extra"
+    t.integer  "response",   default: 0
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
